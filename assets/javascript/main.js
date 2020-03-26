993c6a3cafb3153c42927b0574d6d251e121427f
Love Park <parkloveann@gmail.com>
2020-03-26 15:34:57 -0400

    Add animation API and keyframes

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,19 +1,24 @@
-// selects all elements with animation class
-const images = document.querySelectorAll('.animateDown, .animateUp, .animateLeft, .animateRight, .stagDelay');
+// selects all elements with the animation class
+const items = document.querySelectorAll('.animateDown, .animateUp, .animateLeft, .animateRight, .stagDelay');
 observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         console.log(entries);
         if (entry.intersectionRatio > 0) {
             if (entry.target.classList.contains('animateDown')) {
                 entry.target.style.animation = `animDown 2s ${entry.target.dataset.delay} forwards ease-out`;
+                observer.unobserve(entry.target);
             } else if (entry.target.classList.contains('animateUp')) {
                 entry.target.style.animation = `animUp 2s ${entry.target.dataset.delay} forwards ease-out`;
+                observer.unobserve(entry.target);
             } else if (entry.target.classList.contains('animateLeft')) {
                 entry.target.style.animation = `animLeft 2s ${entry.target.dataset.delay} forwards ease-out`;
+                observer.unobserve(entry.target);
             } else if (entry.target.classList.contains('animateRight')) {
                 entry.target.style.animation = `animRight 2s ${entry.target.dataset.delay} forwards ease-out`;
+                observer.unobserve(entry.target);
             } else if (entry.target.classList.contains('stagDelay')) {
                 entry.target.style.animation = `stagDel 2s ${entry.target.dataset.delay} forwards ease-out`;
+                observer.unobserve(entry.target);
             }
         } else {
             entry.target.style.animation = 'none';
@@ -21,6 +26,6 @@ observer = new IntersectionObserver((entries) => {
     })
 });
 
-images.forEach(image => {
-    observer.observe(image)
+items.forEach(item => {
+    observer.observe(item);
 });
